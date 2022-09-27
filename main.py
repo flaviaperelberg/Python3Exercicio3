5cc0d22714f78d5af5a50f5786c60a5790e1647d
Flávia <113394221+flaviaperelberg@users.noreply.github.com>
2022-09-27 16:35:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Faça um programa que peça dois números e imprima a soma. 
+
+print('Bem-vindos(as) à calculadora de soma Uniruy Wyden!\n')
+numero1=int(input('Digite o primeiro número que gostaria de somar: \n'))
+numero2=int(input('Digite o segunto número que gostaria de somar: \n'))
+print('O resultado da soma é:', numero1+numero2) 
